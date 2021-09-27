ecd2e89a706f92c6f057f18ca344da6372a01ef3
Edmilson Silva <ed1000sons@gmail.com>
2021-09-26 22:37:30 -0300

    build: :sparkles: Add helmet, cors, morgan

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as morgan from 'morgan';
+import * as cors from 'cors';
+import * as helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cors(), helmet(), morgan('dev'));
   await app.listen(3000);
 }
 bootstrap();
